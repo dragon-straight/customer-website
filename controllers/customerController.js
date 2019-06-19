84170d7038b0523f6b938e5f657dc4e6f292b9b4
Pham Hoang Minh <shine020198@gmail.com>
2019-06-20 00:49:09 +0700

    final

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -162,12 +162,16 @@ exports.checkoutCOD_post = function(req,res,){
     });
 };
 
-exports.thank_you = function(req,res){
+exports.thank_you = function(req,res)
+    const manufacturer = productDao.get_Manufacturer();
+    const category = productDao.get_Category();
     var successMsg = req.flash('success')[0];
     res.render('customer/thankyou', {
         pageTitle: 'Cám ơn bạn',
-        successMsg: successMsg
-    })
+        successMsg: successMsg,
+        manufacturerList: await manufacturer,
+        categoryList: await category
+    });
 };
 
 exports.customer_register_get =  function(req, res){
